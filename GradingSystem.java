1d2c881f65e6e22a81bbfb7c7362c83b2805c4c5
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:22:20 +0530

    Update GradingSystem.java

--- GradingSystem.java
+++ GradingSystem.java
@@ -1,1 +1,29 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class GradingSystem {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter your Percentage :-");
+		float per = sc.nextFloat();
+
+		if (per >= 90) {
+			System.out.println("Grade A");
+		} else if (per >= 80 && per < 90) {
+			System.out.println("Grade B");
+		} else if (per >= 70 && per < 80) {
+			System.out.println("Grade C");
+		} else if (per >= 60 && per < 70) {
+			System.out.println("Grade D");
+		} else if (per >= 40 && per < 60) {
+			System.out.println("Grade E");
+		} else {
+			System.out.println("Grade F");
+		}
+	}
+
+}

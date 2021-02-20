6c442f312ee49644171a569ba8e4da4da0d2c83e
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:26:38 +0530

    Update SumOfNaturalNumbers.java

--- SumOfNaturalNumbers.java
+++ SumOfNaturalNumbers.java
@@ -1,1 +1,21 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int res = 0;
+
+		System.out.println("Enter the Range :-");
+		int num = sc.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			res = res + i;
+		}
+		System.out.println("Sum of " + num + " natural numbers is " + res);
+	}
+
+}

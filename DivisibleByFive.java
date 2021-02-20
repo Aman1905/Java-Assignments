ba772cb3f8652de55f3e19995ed12fb5377f7669
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:20:51 +0530

    Update DivisibleByFive.java

--- DivisibleByFive.java
+++ DivisibleByFive.java
@@ -1,1 +1,21 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class DivisibleByFive {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter any Integer :- ");
+		float num = sc.nextFloat();
+
+		if (num % 5 == 0) {
+			System.out.println("Yes !! " + num + " is divisible by 5");
+		} else {
+			System.out.println("Nope !! " + num + " is not divisible by 5");
+		}
+	}
+
+}
 

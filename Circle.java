50afe5dd1b005d7d1267d9385927d6aabb80307a
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:20:30 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,1 +1,22 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the Radius of Circle :- ");
+		float rad = sc.nextFloat();
+
+		float area = (float) ((Math.PI) * rad * rad);
+		System.out.println("Area of Circle = " + area);
+
+		float perimeter = (float) (2 * (Math.PI) * rad);
+		System.out.println("Perimeter of Circle = " + perimeter);
+
+	}
+
+}

bdf2f6bd983bf087a4093bd9466a602130a7fbac
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:23:27 +0530

    Update MilesToKilometers.java

--- MilesToKilometers.java
+++ MilesToKilometers.java
@@ -1,1 +1,19 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class MilesToKilometers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the Miles :- ");
+		float miles = sc.nextFloat();
+
+		float km = (float) (miles * 1.609);
+
+		System.out.println("You have travelled " + km + " Kilometers.");
+	}
+
+}

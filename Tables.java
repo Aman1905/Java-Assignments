4440f0543c3296e04e811838fb00fbd347deac31
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:27:17 +0530

    Update Tables.java

--- Tables.java
+++ Tables.java
@@ -1,1 +1,21 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class Tables {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number :-");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= 10; i++) {
+			int res = i * n;
+			System.out.println(n + " x " + i + " = " + res);
+		}
+
+	}
+
+}

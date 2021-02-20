56c611ecf6b0669189a71e2e304efccc205c2c02
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:23:03 +0530

    Update MaxOf.java

--- MaxOf.java
+++ MaxOf.java
@@ -1,1 +1,31 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class MaxOf {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the 1st number :- ");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter the 2nd number :- ");
+		int b = sc.nextInt();
+		
+		System.out.println("Enter the 3rd number :- ");
+		int c = sc.nextInt();
+		
+		if(a>b && a>c) {
+			System.out.println(a + " is greater than " +b +" and "+c);
+		}
+		else if(b>a && b>c) {
+			System.out.println(b + " is greater than " +a +" and "+c);
+		}
+		else {
+			System.out.println(c + " is greater than " +a +" and "+b);
+		}
+	}
+
+}

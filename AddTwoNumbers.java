a62ee42bb5138c81b606b16224eb2ab0a3450ad6
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:19:39 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,1 +1,20 @@
+package assignments;
 
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the 1st  number :-");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter the 2nd  number :-");
+		int b = sc.nextInt();
+		
+		int sum = a + b;
+		System.out.println("Sum of the "+a+ " and "+b +" is "+ sum );
+	}
+
+}

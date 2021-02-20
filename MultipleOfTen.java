e7d32a40b258acf7d0724520ad7ede7edf22f722
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:23:51 +0530

    Update MultipleOfTen.java

--- MultipleOfTen.java
+++ MultipleOfTen.java
@@ -1,1 +1,25 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class MultipleOfTen {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the 1st Interval :- ");
+		int n1 = sc.nextInt();
+
+		System.out.println("Enter the 2nd Interval :- ");
+		int n2 = sc.nextInt();
+
+		for (int i = n1; i <= n2; i++) {
+			if (i % 10 == 0) {
+				System.out.println("Multiples of 10 => "+i);
+			} 
+		}
+		
+	}
+
+}

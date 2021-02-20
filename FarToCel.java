e3fcba1657974897809bbeedafef5f90aa503777
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:21:33 +0530

    Update FarToCel.java

--- FarToCel.java
+++ FarToCel.java
@@ -1,1 +1,19 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class FarToCel {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the temperature in Fahrenheit :- ");
+		float fahren = sc.nextFloat();
+		
+		float cel = (fahren - 32)*5/9;
+		System.out.println("Temperature in Celsius = "+cel +" C");
+
+	}
+
+}

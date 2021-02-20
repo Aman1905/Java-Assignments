ac7a37ffba45d2060998eb79190e78c1758b9004
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:20:12 +0530

    Update CelToFar.java

--- CelToFar.java
+++ CelToFar.java
@@ -1,1 +1,18 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class CelToFar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the temperature in Celsius :- ");
+		float cel = sc.nextFloat();
+		
+		float fahren = (cel * 9/5)+32;
+		System.out.println("Temperature in Fahrenheit = "+fahren +" F");
+	}
+
+}

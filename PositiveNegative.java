68393a95d6703760353cd0eb65fa4911a852c032
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:24:16 +0530

    Update PositiveNegative.java

--- PositiveNegative.java
+++ PositiveNegative.java
@@ -1,1 +1,22 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the Integer :-");
+		int a = sc.nextInt();
+
+		if (a > 0) {
+			System.out.println(a + " is Positive Integer");
+		} else {
+			System.out.println(a + " is Negative Integer");
+		}
+
+	}
+
+}

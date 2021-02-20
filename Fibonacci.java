fdd4c3765d42c79b8799025f7f940e1a51be7a05
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-20 13:21:58 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,1 +1,26 @@
 
+package assignments;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = 0;
+		int b = 1;
+
+		System.out.println("Enter the number till which you want Fibonacci Sequence :-");
+		int num = sc.nextInt();
+
+		for (int i = 1; i < num + 1; i++) {
+			System.out.println(a);
+			int sum = a + b;
+			a = b;
+			b = sum;
+		}
+
+	}
+
+}

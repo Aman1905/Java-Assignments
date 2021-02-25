91c863a028995d00d97bc3595b808dd90e74e5f3
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-25 21:28:37 +0530

    Exponential

--- Exponential.java
+++ Exponential.java
@@ -1,3 +1,27 @@
 Solution :-
   
   
+package assignments;
+
+import java.util.Scanner;
+
+public class Exponential {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter any Number :-");
+		int num = sc.nextInt();
+
+		System.out.println("Enter the Power :- ");
+		int pow = sc.nextInt();
+
+		int base = num;
+
+		for (int i = 1; i < pow; i++) {
+			base *= num;
+		}
+		System.out.println("Exponent value of the given number is :-" + base);
+
+	}
+
+}

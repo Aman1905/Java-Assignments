e7d2945a4433632072d68baef2ce6115052e3ba9
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-25 21:19:21 +0530

    Buzz

--- Buzz.java
+++ Buzz.java
@@ -1,2 +1,23 @@
 Solution :-
   
+package assignments;
+
+import java.util.Scanner;
+
+public class BuzzOrNot {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number :- ");
+		int num = sc.nextInt();
+
+		if (num % 7 == 0 || num % 10 == 7) {
+			System.out.println(num + " is a Buzz.");
+		} else {
+			System.out.println(num + " is not a Buzz");
+		}
+
+	}
+
+}

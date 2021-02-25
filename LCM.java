436d6e429ca3a76e21ef93f2b7e67d321665d6dc
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-25 21:25:00 +0530

    Update LCM.java

--- LCM.java
+++ LCM.java
@@ -1,3 +1,36 @@
 Solution :-
   
-  
+ 
+package assignments;
+
+import java.util.*;
+
+public class LCM {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int max, step, lcm = 0;
+
+		System.out.println("Enter 1st number :-");
+		int n1 = sc.nextInt();
+
+		System.out.println("Enter 2nd number :-");
+		int n2 = sc.nextInt();
+
+		if (n1 > n2) {
+			max = step = n1;
+		} else {
+			max = step = n2;
+		}
+
+		while (n1 != 0) {
+			if (max % n1 == 0 && max % n2 == 0) {
+				lcm = max;
+				break;
+			}
+			max += step;
+		}
+		System.out.println("LCM of " + n1 + " and " + n2 + " is :- " + lcm);
+	}
+
+}

4a99cc27855b8750460a8f5d4672ee1531c4fd73
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-25 21:27:43 +0530

    Count Digits

--- Count Digits.java
+++ Count Digits.java
@@ -1,3 +1,26 @@
 Solution :-
   
   
+package assignments;
+
+import java.util.*;
+
+public class CountDigit {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number :-");
+		long num = sc.nextInt();
+		//int n1 = num;
+
+		int count = 0;
+
+		while (num != 0) {
+			num /= 10;
+			count++;
+		}
+		System.out.println("The number of digits in the given integer is " + count);
+	}
+
+}

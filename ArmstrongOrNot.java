ccb14decbf8fecf8febc96376ea402930e1cb8a7
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-02-27 17:39:51 +0530

    Update ArmstrongOrNot.java

--- ArmstrongOrNot.java
+++ ArmstrongOrNot.java
@@ -1,2 +1,30 @@
 Solution :-
   
+package assignments;
+
+import java.util.Scanner;
+
+public class ArmstrongOrNot {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int c = 0, a, temp;
+
+		System.out.println("Enter any number :-");
+		int n = sc.nextInt();
+		// It is the number to check armstrong
+		temp = n;
+		while (n > 0) {
+			a = n % 10;
+			n = n / 10;
+			c = c + (a * a * a);
+		}
+		if (temp == c)
+			System.out.println("It is an Armstrong number");
+		else
+			System.out.println("It is Not an Armstrong number");
+
+	}
+
+}
